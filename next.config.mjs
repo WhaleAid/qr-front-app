00e1f398c6df0b3305c6f40b2563b2169f291146
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-08-28 15:50:20 +0200

    fix local storage redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            // {
-            //     source: '/authentication',
-            //     has: [
-            //         {
-            //             type: 'cookie',
-            //             key: 'accessToken',
-            //         },
-            //         {
-            //             type: 'cookie',
-            //             key: 'refreshToken',
-            //         },
-            //     ],
-            //     permanent: false,
-            //     destination: '/dashboard',
-            // },
-            {
-                source: '/((?!api|_next/static|_next/image|favicon.ico|authentication|register|assets).*)',
-                missing: [
-                    {
-                        type: 'localStorage',
-                        key: 'accessToken',
-                    },
-                ],
-                permanent: false,
-                destination: '/authentication',
-            }
-        ];
-    }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-"use client"
+"use client";
 
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./storeProvider";
@@ -8,9 +10,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const publicPaths = ["/authentication"];
+    const pathRequiresAuth = !publicPaths.includes(pathname);
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (pathRequiresAuth && !accessToken) {
+      router.push("/authentication");
+    } else if (pathname === "/authentication" && accessToken) {
+      router.push("/dashboard");
+    }
+  }, [router]);
+
   return (
     <StoreProvider>
       <html lang="en">
