500d6d656088ac68f2eeb976df3f91ac5347dca9
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-10-18 13:10:16 +0200

    fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout(props: { children: React.ReactNode }) {
     return (
         <main className="flex min-h-screen h-full flex-col items-center lg:p-16 p-6 relative overflow-hidden">
             <div className="w-full relative flex dark:bg-gray-800 bg-[#f7f5ff] rounded-xl shadow-md transition-all lg:h-screen-minus h-screen-minus-mobile">
-                <SideBar />
+                {/* <SideBar /> */}
                 {props.children}
             </div>
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
       <main className="flex min-h-screen h-full flex-col items-center lg:p-16 p-6 relative">
         <div className="w-full dark:bg-gray-800 bg-white rounded-xl shadow-md transition-all h-screen-minus overflow-x-hidden">
-          {/* <SideBar /> */}
+          <SideBar />
         </div>
       </main>
   );
